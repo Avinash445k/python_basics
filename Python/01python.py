b833aecbeb88df6d4fe68acf50a4d267d774603a
Avinash445k <avinash0003041997@gmail.com>
2024-11-17 21:41:33 +0530

    python file update and code update

--- Python/01python.py
+++ Python/01python.py
@@ -4,4 +4,13 @@
 
 if 5 > 2:
     print ("Five is greater than two")
+if 4 > 2:
+        print ("Four is greater than two")
 
+if 5 > 2:
+    print ("Five is greater than two")
+    print ("Four is greater than two")
+
+if 3 > 2:
+        print ("three is greater than two")
+        print ("three is greater than two")
---
+++ Python/02python.py
@@ -0,0 +1,13 @@
+# Variables Used
+x = 6
+y = "Hello, World"
+z = 23 
+
+print (x)
+print (y)
+print (z)
+
+# Comments Used 
+
+# this is a Comment
+print ("This is my First Python Programming language")
